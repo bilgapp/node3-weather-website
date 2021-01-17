3f4458efbefaddc23630fafe6cdf255223d7efd6
Amihai Shapira <amihai@apple.com>
2021-01-17 09:15:01 +0200

    bug fix

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
-const port = process.env.port || 3000
+const port = process.env.PORT || 3000
 
 // define paths for express config
 const publicPath = path.join(__dirname, "../public")
