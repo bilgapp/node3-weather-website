0ed492bd6934a0941a8716dd4c920857d298d28e
Amihai Shapira <amihai@apple.com>
2021-01-17 09:05:41 +0200

    Setup app for Heroko

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const forecast = require('./utils/forecast')
 const geocode = require('./utils/geocode')
 
 const app = express()
+const port = process.env.port || 3000
 
 // define paths for express config
 const publicPath = path.join(__dirname, "../public")
@@ -97,6 +98,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
